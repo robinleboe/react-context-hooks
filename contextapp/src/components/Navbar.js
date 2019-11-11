7ab052f3ad99064b5891b4e8aefcb22448ca3924
Robin Leboe <robin.leboe@gmail.com>
2019-11-10 19:47:10 -0800

    context via static contextType

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
-    render() { 
-        return ( 
-            <nav>
-                <h1>
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                    </ul>
-                </h1>
-            </nav>
-         );
-    }
+  static contextType = ThemeContext;
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <nav style={{ background: theme.ui, color: theme.syntax}}>
+        <h1>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </h1>
+      </nav>
+    );
+  }
 }
- 
+
 export default Navbar;
