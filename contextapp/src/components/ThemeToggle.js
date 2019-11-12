c1c2088b593831450f2b3de08fa9852973effcfd
Robin Leboe <robin.leboe@gmail.com>
2019-11-11 20:07:28 -0800

    update classes to functional components with hooks

--- contextapp/src/components/ThemeToggle.js
+++ contextapp/src/components/ThemeToggle.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
+import React, { useContext } from "react"; // use { Component } for class version
+import { ThemeContext } from "../contexts/ThemeContext";
 
-class ThemeToggle extends Component {
-    static contextType = ThemeContext;
-    render() { 
-        const { toggleTheme } = this.context;
-        return ( 
-            <button onClick={toggleTheme}>Toggle the theme</button>
-         );
-    }
-}
- 
+// class ThemeToggle extends Component {
+//     static contextType = ThemeContext;
+//     render() {
+//         const { toggleTheme } = this.context;
+//         return (
+//             <button onClick={toggleTheme}>Toggle the theme</button>
+//          );
+//     }
+// }
+
+const ThemeToggle = () => {
+  const { toggleTheme } = useContext(ThemeContext);
+  return <button onClick={toggleTheme}>Toggle the theme</button>;
+};
+
 export default ThemeToggle;
